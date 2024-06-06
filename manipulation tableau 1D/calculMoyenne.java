bbbd9f2227feb4e4cb3ec703981fc8642933f69c
Clarisse Otmani <clarisseotm@gmail.com>
2024-06-06 08:40:33 +0200

    Modification de CalculMoyenne

--- manipulation tableau 1D/calculMoyenne.java
+++ manipulation tableau 1D/calculMoyenne.java
@@ -1,11 +1,13 @@
 
-public class calculMoyenne {
+public class CalculMoyenne {
     public static void main(String[] args) {
         // tableau d'entiers
         int[] array = { 1, 2, 5, 4 };
         int total = 0;
         for (int i = 0; i < array.length; i++) {
-total = total + array[i];}
-double moyenne = total/array.length;
-   System.out.println("La moyenne est :" + moyenne); }
+            total = total + array[i];
+        }
+        double moyenne = total / array.length;
+        System.out.println("La moyenne est :" + moyenne);
+    }
 }
